179d73ab5257c69b662469ed9a88056e6c82d17d
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2020-11-24 08:41:03 -0800

    https://www.codewars.com/kata/5533c2a50c4fea6832000101

---
+++ kata-7kyu/dictionary-from-two-lists.js
@@ -0,0 +1,9 @@
+function createDict(keys, values){
+  let obj = {};
+  for(let i = 0; i < keys.length; i++)
+  if(i < values.length)
+    obj[keys[i]] = values[i];
+  else
+    obj[keys[i]] = null;
+  return obj;
+}
