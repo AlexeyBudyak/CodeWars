f7f393475bae8b1186efe5fbb971448a34395f0e
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2020-12-03 20:05:15 -0800

    Create Is-a-number-prime.js

---
+++ kata-6kyu/Is-a-number-prime.js
@@ -0,0 +1,7 @@
+function isPrime(num) {
+  if(num <= 1)  return false;
+  const root = Math.floor(Math.sqrt(num));
+    for(let i = 2; i <= root; i++) 
+        if(num % i == 0) return false;
+  return true;
+}
