e8844e5f54f24cd2c660d7bf22829d5c8b8d3f52
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-01-15 01:05:59 -0800

    Create array-helpers.js

---
+++ kata-6kyu/array-helpers.js
@@ -0,0 +1,18 @@
+Array.prototype.square = function(){
+  return this.map(el => el * el);
+}
+Array.prototype.cube = function(){
+  return this.map(el => el ** 3);
+}
+Array.prototype.sum = function(){
+  return this.reduce((sum, x) => sum + x, 0);
+}
+Array.prototype.average = function(){
+  return this.sum() / this.length;
+}
+Array.prototype.even = function(){
+  return this.filter(el => el % 2 === 0);
+}
+Array.prototype.odd = function(){
+  return this.filter(el => el % 2);
+}
