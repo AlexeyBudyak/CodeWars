15d7b1c6f83a591236a70f151d54a62acc33b623
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-01-23 00:44:49 -0800

    Create for-UFC-fans-(total-beginners)-conor-mccregor-vs-george-saint-pierre.js

---
+++ kata-8kyu/for-UFC-fans-(total-beginners)-conor-mccregor-vs-george-saint-pierre.js
@@ -0,0 +1,4 @@
+var quote = function(fighter) {
+  return fighter.toLowerCase().includes("a") ? "I am not impressed by your performance."
+                   : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+}
