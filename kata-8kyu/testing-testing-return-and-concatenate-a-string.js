a9059787e0e498cfb3b8b96cb06ed18978c0711a
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-02-02 13:51:56 +0200

    Create testing-testing-return-and-concatenate-a-string.js

---
+++ kata-8kyu/testing-testing-return-and-concatenate-a-string.js
@@ -0,0 +1,5 @@
+function getRes(myScore) {
+  return (myScore >= 70 ? 'Congratulations, you have passed!'
+                       :  'Sorry, you have failed. Better luck next time!')
+                       +  ' Thank you for taking part.';
+}
