5dbac3dd15d8675cf99cceab455ababf8bf8d86d
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-04-11 01:50:17 -0700

    Create alan-partridge-III-london.js

---
+++ kata-7kyu/alan-partridge-III-london.js
@@ -0,0 +1,5 @@
+function alan(x){
+  const stations = ['Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway'];
+  return stations.every(el => x.includes(el)) ? 'Smell my cheese you mother!'
+                                              : 'No, seriously, run. You will miss it.';
+}
