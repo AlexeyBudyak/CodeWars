e61d060cd1862040b742b20f3476ee933b661327
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-04-25 01:56:16 -0700

    Create harvest-festival.js

---
+++ kata-7kyu/harvest-festival.js
@@ -0,0 +1,4 @@
+function plant(seed, water, fert, temp){
+  return temp < 20 || temp > 30 ? '-'.repeat(water ** 2) + seed
+                                  : ('-'.repeat(water) + seed.repeat(fert)).repeat(water)
+}
