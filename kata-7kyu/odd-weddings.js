6417ecec44e827b77902bbee38764761ccee08a3
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-05-08 15:15:57 -0700

    Create odd-weddings.js

---
+++ kata-7kyu/odd-weddings.js
@@ -0,0 +1,5 @@
+function checkMarriedNum(n){
+  if(n < 0) return "Not to cause any trouble but shouldn't that be a positive number?"
+  if(n % 2) return "Not to cause any trouble but shouldn't that be an even number?"
+  return "Sound fair"
+}
