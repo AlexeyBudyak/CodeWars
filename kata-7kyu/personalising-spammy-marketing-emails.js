8d04a49b82cc52038b9192cbbd3bb316f865b01b
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-05-23 23:15:30 -0700

    Create personalising-spammy-marketing-emails.js

---
+++ kata-7kyu/personalising-spammy-marketing-emails.js
@@ -0,0 +1,5 @@
+function personalise( campaign, person){
+  return campaign.replace(/<NAME>/g, person.name)
+                 .replace(/<CITY>/g, person.city)
+                 .replace(/<FAVOURITE PRODUCTS>/g, person.favourite_products);
+}
