f44a3f31438164cc981f9fdf932b8a2be6fd0c4a
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-06-03 23:15:43 -0700

    Update make-a-spiral.py

--- python-kata-3kyu/make-a-spiral.py
+++ python-kata-3kyu/make-a-spiral.py
@@ -1,17 +1,17 @@
-def step(d, x, y):
-    x+= (d % 2 - 1) * (d % 4 -1)
-    y+= -(d % 2) * (d % 4 - 2)
+def step(d, x, y): 
+    x+= (d % 2 - 1) * (d - 1)
+    y+= -(d % 2) * (d - 2)
     return (x,y)
 
 def spiralize(size):
     spiral = list([1] * size for i in range(size))
-    x = 0; y = 1; d = 0 # d - direction, 0 - East, 1 - South, 2 - West, 3 North
-    while True:
+    x2 = x = 0; y2 = y = 1; d = 0 
+    while spiral[y2][x2]:
         spiral[y][x] = 0
         [x2,y2] = step(d, x, y)
-        if(spiral[y2][x2] == 0):    return spiral
         [x3,y3] = step(d, x2, y2)
         if max(x3, y3) == size or min(x3, y3) == -1 or spiral[y3][x3] == 0:
-            d+= 1
+            d= (d + 1) % 4
         else: 
             [x, y] = [x2, y2]
+    return spiral
