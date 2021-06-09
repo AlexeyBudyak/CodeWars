43d76f3edf367affeef78edec1de5d8597fb526b
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-06-08 18:40:07 -0700

    Update hidden-cubic-numbers.py

--- python-kata-6kyu/hidden-cubic-numbers.py
+++ python-kata-6kyu/hidden-cubic-numbers.py
@@ -1,25 +1,24 @@
-def is_cubic(s):
-    n = 0
-    for el in list(s):
-        n+= int(el) ** 3
-    return n == int(s)
-
 def is_sum_of_cubes(s):
-    nums = []
-    num = ''
+    num_str = ''
+    num_digit = 0
+    num_cubic = 0
+    num_len = 0
     total = 0
     lucky = ''
     for el in s+' ':
-        if el in '0123456789' and len(num) < 3:
-            num+= el
-        if (num != '' and el not in '0123456789') or len(num) == 3:
-            nums.append(num)
-            num = ''
-            
-    for i in nums:
-        if is_cubic(i): 
-            lucky += (i + ' ')
-            total+= int(i)
+        is_digit = el in '0123456789'
+        if is_digit and num_len < 3:
+            num_str+= el
+            num_len+= 1
+            num_cubic+= int(el) ** 3
+        if (num_str and not is_digit) or num_len == 3:
+            num_digit = int(num_str)
+            if num_digit == num_cubic: 
+                lucky+= num_str + ' '
+                total+= num_digit
+            num_str = ''
+            num_cubic = 0 
+            num_len = 0
             
     if lucky != '': return lucky + str(total) + ' Lucky'
     return "Unlucky"
