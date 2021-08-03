836f9f2550cabd16d9af38a04bb0145999d95e5a
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-08-03 00:00:59 -0700

    Update coding-3min-special-factor.py

--- python-kata-7kyu/coding-3min-special-factor.py
+++ python-kata-7kyu/coding-3min-special-factor.py
@@ -1,6 +1,10 @@
 def sc(n): 
     factors = []
+    special_factors = []
     for x in range(1, n+1):
         if n % x == 0:
             factors.append(x)
-    return factors
+    for x in factors:
+        if str(bin(x))[2:] in str(bin(n))[2:]:
+            special_factors.append(x)
+    return special_factors
